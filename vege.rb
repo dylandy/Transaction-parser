2c5da3cd0ee5424fc2f2339b691af4b62c9cf35f
Dylandy Chang <steven80919@gmail.com>
2013-11-23 17:47:53 +0800

    fix error

--- vege.rb
+++ vege.rb
@@ -1,50 +1,22 @@
 # encoding:utf-8
 
-require 'rubygems' #載入基本 ruby gems
-require 'active_record' #資料庫處理需要
+require 'rubygems' 
+require 'active_record' 
 require 'nokogiri'
-require 'yaml' #需要處理 yml 檔案
 require 'date'
 require 'open-uri'
 require 'pp'
+require 'yaml'
 
-class Catalog < ActiveRecord::Base ; end #需要使用到的資料庫物件，先繼承自 ActiveRecord
+class Catalog < ActiveRecord::Base ; end 
 class Vegetable < ActiveRecord::Base ; end 
 class VegetableLog < ActiveRecord::Base ; end
 
 module Robot
-  DB_CONNECTION_SETTING = {
-    adapter: "mysql2",
-	  encoding: "utf8",
-	  database: "project_development",
-	  pool: 5,
-	  username: "root",
-	  password: '0000',
-		socket: "/var/run/mysqld/mysqld.sock"
-	}
-  THREAD_COUNT = 10
   def self.initialize
-    ActiveRecord::Base.establish_connection(DB_CONNECTION_SETTING)
-=begin
-    @@pool = [] 	#宣告一個 pool 用來存放所有的 thread 空間
-    @@semaphore = Mutex.new
-=end
-  end
-=begin
-	def self.set_thread(start_at)		
-	  is_using_thread = false
-	  (0...THREAD_COUNT).each do |i|
-		  if !@@pool[i] || !@@pool[i].status
-			  @@pool[i] = Thread.new do
-			    vegetable_filter(start_at)
-				end
-				is_using_thread = true
-				break
-			end
-		end
-		return is_using_thread
-	end
-=end
+    environment = ENV['RACK_ENV'] || 'development'
+    dbconfig = YAML.load(File.read('database.yml')) #change the path of your own database.yml 
+    ActiveRecord::Base.establish_connection dbconfig[environment]
   def self.go! 
     initialize()
     vegetable()
@@ -58,24 +30,18 @@ module Robot
       Vegetable.select('id , serial').each do |v|
          @@all_vegetable_set[v.serial.strip] = v.id
       end
-
-	#	pp all_vegetable_set
     if start_at == end_at 
-       puts "Already done the parsing"
+     puts "Already done the parsing"
     end
+
     while start_at < end_at do
-#		  is_threaded = set_thread(start_at)
-#     if is_threaded
         start_at = start_at.next_day
         vegetable_filter(start_at)
-#		 else
-        sleep(0.1)  #設定每 1 秒才讀一次目標網站
-#		  end
+        sleep(0.1)  
     end
+    #sleep 0.1 sec between 2 times reading the  target website 
   end
 	def self.get_vegetable_id(serial , name , r_name)
-#	  @@semaphore.synchronize do
-		
 	  serial = serial.strip
 	  if @@all_vegetable_set[serial]
 		  return @@all_vegetable_set[serial]
@@ -88,8 +54,8 @@ module Robot
 			@@all_vegetable_set[serial] = v.id
 			return v.id
 	  end
-#		end
 	end
+
   def self.vegetable_filter(date)
     year = (date.strftime('%Y').to_i - 1911).to_s.rjust(3 , '0')
     puts "http://www.tapmc.com.tw/tapmc_new16/price1.asp?YEARS=#{year}&MONTHS=#{date.strftime('%m')}&DAYS=#{date.strftime('%d')}&FV_CODE=A&MARKET=1&temp=Z"
